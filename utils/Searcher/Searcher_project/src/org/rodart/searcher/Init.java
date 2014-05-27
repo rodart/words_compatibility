e4922484b6e0567d1cb72cc41067ad775b1f8bd9
Artem Rodichev <rodichev@newstar.rinet.ru>
2014-05-27 10:31:53 +0400

    Searcher modif

--- utils/Searcher/Searcher_project/src/org/rodart/searcher/Init.java
+++ utils/Searcher/Searcher_project/src/org/rodart/searcher/Init.java
@@ -1,7 +1,14 @@
 package org.rodart.searcher;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.List;
 
@@ -11,12 +18,29 @@ import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
 
 public class Init {
 
+    private static String getQueryWord(String fileName) {
+        File file = new File(fileName);
+        String query = new String();
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream (file), "UTF8");
+            BufferedReader bufferedReader = new BufferedReader (inputStreamReader);
+            query = bufferedReader.readLine();
+            bufferedReader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return query;
+    }
+    
 	public static void main(String[] args) throws IOException, ParseException, InvalidTokenOffsetsException {
 	    String dirName = args[0];
 		String resultFilename = args[1];
-		String query_str = args[2];
+		String queryFileName = args[2];
 	    Integer max_hits_number = Integer.valueOf(args[3]);		
 		
+	    String query_str = getQueryWord(queryFileName);
+	    System.out.println("Try to process query " + query_str);
+	    
 		SearchBook searchBook = new SearchBook(dirName);
 		List<String> resultSentences = searchBook.search(query_str, max_hits_number);
 		
@@ -25,12 +49,20 @@ public class Init {
 		}
 		
 		try {
-			PrintWriter out = new PrintWriter(new FileWriter(resultFilename));
-			out.println(query_str);
-	        for (String sentence : resultSentences) { 
-	        	out.println(sentence);
+		    File file = new File(resultFilename);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+            
+            bufferedWriter.write(query_str);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+	        
+            for (String sentence : resultSentences) { 
+                bufferedWriter.write(sentence);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
 	        }
-			out.close();
+	        bufferedWriter.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
